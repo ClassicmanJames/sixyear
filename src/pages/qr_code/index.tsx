18b4175017e1eb852722540cd2a1b333d065218b
Classicman <jamesr.com@gmail.com>
2024-01-15 11:06:49 +0700

    up Qr

--- src/pages/qr_code/index.tsx
+++ src/pages/qr_code/index.tsx
@@ -1,53 +1,68 @@
 import router from "next/router";
-import { useQRCode } from 'next-qrcode';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-
+import { useQRCode } from "next-qrcode";
+import Image from "next/image";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import * as Setting from "@/redux/reducers/setting";
 
 export default function Page() {
-    const { SVG } = useQRCode();
-    const [tkData, setTkData] = useState([]);
-
-
-    useEffect(() => {
-        const getToken = async () => {
-            console.log("22222   " + process.env.NEXT_PUBLIC_CMU_SERVICE);
-            const response = await axios.post(process.env.NEXT_PUBLIC_CMU_SERVICE + '/setSertTK', { cit_id: '1103703736990' }, {
-                headers: {
-                    "content-type": "application/json",
-                }
-            });
-            setTkData(response.data.token);
-            console.log(response.data);
+  const { SVG } = useQRCode();
+  const [tkData, setTkData] = useState([]);
+  const dispath = useDispatch();
+  const getUser = useSelector((state: any) => state.user);
+  //console.log(getUser.accessToken);
+  dispath(Setting.updateState({ isLoadingScreen: false }));
+  useEffect(() => {
+    const getToken = async () => {
+      console.log("22222   " + process.env.NEXT_PUBLIC_CMU_SERVICE);
+      const response = await axios.post(
+        process.env.NEXT_PUBLIC_CMU_SERVICE + "/setSertTK",
+        { cit_id: getUser.accessToken },
+        {
+          headers: {
+            "content-type": "application/json",
+          },
         }
-        getToken();
-    }, []);
+      );
+      setTkData(response.data.token);
+      console.log(response.data);
+    };
+    getToken();
+  }, []);
 
+  return (
+    <div className="m-5 h-50 bg-[#FBFBFB] drop-shadow-xl rounded-xl p-2">
+      <div className="grid m-5  rounded-lg justify-items-center">
+        <Image
+          src={"/SUBLOGO1.png"}
+          width={"200"}
+          height={"30"}
+          alt={"ssd"}
+        ></Image>
+      </div>
+      <div className="h-50  rounded-lg  text-green-500  text-center">
+        <div className="p-2 rounded-lg text-base">
+          <div className="p-2 bg-green-100 rounded-lg">
+            โปรดแสดง Qr Code นี้แก่เจ้าหน้าก่อนเข้างาน <br /> (Please show this
+            Qr Code to the staff before entering the event.)
+          </div>
 
-
-
-    return (
-        <div>
-            <p className="text-4xl font-semibold text-gray-900 dark:text-white">QR Code</p>
-            <div className="flex h-screen">
-            <div className="m-auto">
-                <SVG text={tkData.toString() ||'3'}
-                    options={{
-                        margin: 2,
-                        width: 200,
-                        color: {
-                            dark: '#000000',
-                            light: '#FFFFFF',
-                        },
-                    }}
-                />
-            </div>
-            </div>
-
-            
+          <div className="flex justify-center">
+            <SVG
+              text={tkData.toString() || "3"}
+              options={{
+                margin: 2,
+                width: 200,
+                color: {
+                  dark: "#000000",
+                  light: "#FFFFFF",
+                },
+              }}
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
-
-

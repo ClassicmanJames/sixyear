457249b24765b5a43ac4e810c5c9f89b06861537
suntrory <taragornatid@gmail.com>
2024-01-17 13:25:20 +0700

    qrcode scanner

---
+++ src/pages/qr_code/index copy.tsx
@@ -0,0 +1,50 @@
+import router from "next/router";
+import { useQRCode } from 'next-qrcode';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useParams, useSearchParams } from "next/navigation";
+
+export default function Page() {
+
+    const searchParams = useSearchParams();
+    const { SVG } = useQRCode();
+    const [tkData, setTkData] = useState([]);
+    const cit_id = searchParams.get("citizen_id") || '1509966253746';
+
+    useEffect(() => {
+        const getToken = async () => {
+            console.log("22222   " + process.env.NEXT_PUBLIC_CMU_SERVICE);
+            const response = await axios.post(process.env.NEXT_PUBLIC_CMU_SERVICE + '/setSertTK', { cit_id: cit_id }, {
+                headers: {
+                    "content-type": "application/json",
+                }
+            });
+            setTkData(response.data.token || '3');
+            console.log(response.data);
+        }
+        getToken();
+    }, []); 
+
+    return (
+        <div>
+            <p className="text-4xl font-semibold text-gray-900 dark:text-white">QR Code</p>
+            <div className="flex h-screen">
+                <div className="m-auto">
+                    <SVG text={tkData.toString() ||'3'}
+                        options={{
+                            margin: 2,
+                            width: 200,
+                            color: {
+                                dark: '#000000',
+                                light: '#FFFFFF',
+                            },
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
--- src/pages/qr_code_scanner/index.tsx
+++ src/pages/qr_code_scanner/index.tsx
@@ -6,62 +6,135 @@ import axios from 'axios';
 export default function Page() {
 
   const [scanResult, setScanResult] = useState("");
+  const [showPass, setShowPass] = useState(false);
+  const [showErr, setShowErr] = useState(false);
+  const [txtStamp, setTxtStamp] = useState("");
+  var html5QrCode:Html5Qrcode;// = new Html5Qrcode("reader");
+  
 
 
   useEffect(() => {
 
+    let dateSmp = new Date();
+    //  + dateSmp.getDate() + "/" + (dateSmp.getMonth() +1) + "/" + dateSmp.getFullYear() + " "
+    setTxtStamp("เข้างาน : " + dateSmp.getHours() + ":" + dateSmp.getMinutes() + ":" + dateSmp.getSeconds());
 
-    const html5QrCode = new Html5Qrcode("reader");
+    //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaXRfaWQiOiIxMTQ5NTAwMDA1NTU0IiwiaWF0IjoxNzA1Mzc3MTQxLCJleHAiOjE3MDYyNDExNDF9.F_VR3_7bEpJLFDbMAI4rJK3fVhRXRTq3P_Cr1H9hbho";   
     const qrCodeSuccessCallback = async (decodedText: any, decodedResult: any) => {
-      /* handle success */
-      //setScanResult(decodedText);
-      console.log(decodedText);
-      let data = {
-        'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaXRfaWQiOiIxMTAzNzAzNzM2OTkwIiwiaWF0IjoxNzA1MDUyMDY5LCJleHAiOjE3MDUxMzg0Njl9.VkXgyvpkxDmq6HdXRv5aWdZNiNpERlJGP1lfj4UFP4E' 
-      };
-      
-      let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://60th.scmc.cmu.ac.th/verifyToken',
-        headers: { 
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data : data
-      };
-      
-      axios.request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        let resData = response.data;
-        if(resData.status == 1){
-          setScanResult("PASS");
-        }
+      if(decodedText){
+        let data = {
+            token: decodedText
+          }
+        axios.post(
+          process.env.NEXT_PUBLIC_CMU_SERVICE + `/checkIn`,
+          data,
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        ).then((response) => {
+          console.log(JSON.stringify(response.data));
+          let resData = response.data;
+          if(resData.status == 1){
+            setScanResult(`<h1 className="bg-[#50d71e] w-full h- 20 text-3xl">ผ่าน</h1>`);
+          }else{
+            setScanResult(`<h1 className="bg-[#ff3333] w-full h- 20 text-3xl">ไม่ผ่าน</h1>`);
+          }
+          html5QrCode.pause();
+          //html5QrCode.stop();
+          html5QrCode.clear();
+          setShowPass(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
         
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-      
-
-      html5QrCode.clear();
-
-
+        //html5QrCode.clear();
+      }
     };
-    const config = { fps: 2, qrbox: { width: 250, height: 250 } };
-    html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback, (err) => { });
+    const config = { fps: 4, qrbox: { width: 250, height: 250 }};
+    html5QrCode = new Html5Qrcode("reader");
+    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, (err) => { });
 
   }, [])
 
+  //////////
+  const openCame = () => {
+    if(html5QrCode){
+      html5QrCode.resume();
+    }else{              
+      const qrCodeSuccessCallback = async (decodedText: any, decodedResult: any) => {
+        if(decodedText){
+          let data = {
+              token: decodedText
+            }
+          axios.post(
+            process.env.NEXT_PUBLIC_CMU_SERVICE + `/checkIn`,
+            data,
+            {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+            }
+          ).then((response) => {
+            console.log(JSON.stringify(response.data));
+            let resData = response.data;
+            if(resData.status == 1){
+              setScanResult(`<h1 className="bg-[#50d71e] w-full h- 20 text-3xl">ผ่าน</h1>`);
+              setShowPass(true);
+            }else if(resData.status == 2){
+              let dateSmp = new Date(resData.stamp);
+              //+ dateSmp.getDate() + "/" + (dateSmp.getMonth() +1) + "/" + dateSmp.getFullYear() + " " 
+              setTxtStamp("เข้างาน : " + dateSmp.getHours() + ":" + dateSmp.getMinutes() + ":" + dateSmp.getSeconds());
+              setShowPass(false);
+            }
+            else{
+              setScanResult(`<h1 className="bg-[#ff3333] w-full h- 20 text-3xl">ไม่ผ่าน</h1>`);
+            }
+            html5QrCode.pause();
+            //html5QrCode.stop();
+            html5QrCode.clear();
+            
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+          
+          //html5QrCode.clear();
+        }  
+      };
+      const config = { fps: 4, qrbox: { width: 250, height: 250 }};
+      html5QrCode = new Html5Qrcode("reader");
+      html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, (err) => { });
+    }
+  }
+
+  const tHint = (state: boolean) => {
+    setShowPass(state);
+    setShowErr(state);
+  }
 
   return (
     <div
       className={"grid m-5 h-50"}>
       <div className="justify-center col">
         <div className="" id="reader"></div>
-        <div className="" >
-          <h1>{scanResult}</h1>
+        <div style={{ display: showPass ? "block" : "none" }} className="text-center my-4">
+          <h1 className="bg-[#66CC00] w-full h- 20 text-3xl p-2 text-[#0E275C]">ผ่าน</h1>
         </div>
+        <div style={{ display: showErr ? "block" : "none" }} className="text-center my-4">
+          <h1 className="bg-[#EB3E3E] w-full h- 20 text-3xl p-2 text-[#292929]">ไม่ผ่าน</h1>
+        </div>
+        <div className="flex justify-center my-4 h-auto" >
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-56 h-14 rounded-lg" onClick={() => openCame()} >
+            RESET
+          </button>
+        </div>
+        <div className="flex justify-left my-4 h-auto" >
+          <p className="">{txtStamp}</p>
+        </div>
+        
         
       </div>
     </div>
